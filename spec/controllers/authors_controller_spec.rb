b152bac8082a9d7027530f13168be0d5788e9fb9
Bartek Wilczek <bwilczek@gmail.com>
2017-09-30 19:28:27 +0200

    Make specs green

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -28,13 +28,9 @@ RSpec.describe AuthorsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Author. As you add validations to Author, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) { { name: 'Kasia' } }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) { { name: nil } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -96,15 +92,13 @@ RSpec.describe AuthorsController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) { { name: 'Marzena' } }
 
       it "updates the requested author" do
         author = Author.create! valid_attributes
         put :update, params: {id: author.to_param, author: new_attributes}, session: valid_session
         author.reload
-        skip("Add assertions for updated state")
+        expect(author.name).to be_eql('Marzena')
       end
 
       it "redirects to the author" do
