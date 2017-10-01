255af226e82da070f7d263113527e28ffcfcb522
Bartek Wilczek <bartekwilczek@workfront.com>
2017-10-01 12:36:00 +0200

    Added more specs for LibraryStats

--- spec/lib/library_stats_spec.rb
+++ spec/lib/library_stats_spec.rb
@@ -13,4 +13,25 @@ RSpec.describe LibraryStats do
     avg = LibraryStats.avg_page_count([book1, book2])
     expect(avg).to be_eql(200.0)
   end
+
+  it 'average for one entry with mocked pages method' do
+    book = build(:book)
+    allow(book).to receive(:pages) { 300 }
+    avg = LibraryStats.avg_page_count(Array(book))
+    expect(avg).to be_eql(300.0)
+  end
+
+  it 'should call pages method on book object' do
+    book = build(:book)
+    allow(book).to receive(:pages) { 300 }
+    LibraryStats.avg_page_count(Array(book))
+    expect(book).to have_received(:pages)
+  end
+
+  it 'average for two test double entries' do
+    book1 = double(:book1, pages: 100)
+    book2 = double(:book2, pages: 300)
+    avg = LibraryStats.avg_page_count([book1, book2])
+    expect(avg).to be_eql(200.0)
+  end
 end
